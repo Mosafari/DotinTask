cb462088cedc430678e968715a3874641688b8f1
mosafari <m.safari2680@gmail.com>
2023-09-01 16:01:02 +0330

    store data in a map

--- TestOne.java
+++ TestOne.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader; // to read from file
 import java.io.IOException; // Import this class to handle errors
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestOne{
     public static void main(String[] args) {
@@ -9,9 +11,35 @@ public class TestOne{
             line = reader.readLine();
             String[] data = line.split(" "); //split one line by " "(space)
             
-            for(String d: data){
-                System.out.println(d); // print each element
-            }
+            // store data in hashmap 
+            int i = 1;
+            Map<String, Map<String, String>> cleandata = new HashMap<>();
+            Map<String, String> threaddata ;
+            
+            threaddata = new HashMap<>();
+            threaddata.put("startdate" + i, data[0] +" "+ data[1]);            
+            threaddata.put("status" + i, data[4]);
+
+            // Checking status
+            System.out.println(data[4].equals("starting") );
+            if( data[4].equals("starting") ) {
+                threaddata.put("body" + i, data[5]+data[6]+data[7]);
+                cleandata.put(data[2], threaddata);
+                
+            }else if ( data[4].equals("processing") ){
+                threaddata.put("status" + i, data[4]);
+                threaddata.put("processtime", data[0] + " " + data[1]);
+
+            // continue
+            }else if ( data[4].equals("end") ){
+                threaddata.put("endcode" + i, data[5]);
+                threaddata.put("endtime", data[0] + " " + data[1]);
+                // Del after add to file
+                threaddata.remove(data[2]);
+                }
+                System.out.println(cleandata);
+                System.out.println(threaddata);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
