e629fff9cda20e1c2f948604aa8a095846db9a5b
mosafari <m.safari2680@gmail.com>
2023-09-01 16:11:03 +0330

    test of 10 line of data

--- TestOne.java
+++ TestOne.java
@@ -8,38 +8,37 @@ public class TestOne{
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new FileReader("trx.txt"))) {
             String line;
-            line = reader.readLine();
-            String[] data = line.split(" "); //split one line by " "(space)
-            
+
             // store data in hashmap 
-            int i = 1;
             Map<String, Map<String, String>> cleandata = new HashMap<>();
             Map<String, String> threaddata ;
-            
-            threaddata = new HashMap<>();
-            threaddata.put("startdate" + i, data[0] +" "+ data[1]);            
-            threaddata.put("status" + i, data[4]);
 
-            // Checking status
-            System.out.println(data[4].equals("starting") );
-            if( data[4].equals("starting") ) {
-                threaddata.put("body" + i, data[5]+data[6]+data[7]);
-                cleandata.put(data[2], threaddata);
-                
-            }else if ( data[4].equals("processing") ){
-                threaddata.put("status" + i, data[4]);
-                threaddata.put("processtime", data[0] + " " + data[1]);
+            for (int i=0; i < 10;i++){
+                line = reader.readLine();
+                String[] data = line.split(" "); //split one line by " "(space)
+                threaddata = new HashMap<>();
+                threaddata.put("startdate" + data[2], data[0] +" "+ data[1]);            
+                threaddata.put("status" + data[2], data[4]);
 
-            // continue
-            }else if ( data[4].equals("end") ){
-                threaddata.put("endcode" + i, data[5]);
-                threaddata.put("endtime", data[0] + " " + data[1]);
-                // Del after add to file
-                threaddata.remove(data[2]);
-                }
-                System.out.println(cleandata);
-                System.out.println(threaddata);
+                // Checking status
+                if( data[4].equals("starting") ) {
+                    threaddata.put("body" + data[2], data[5]+data[6]+data[7]);
+                    cleandata.put(data[2], threaddata);
+                    
+                }else if ( data[4].equals("processing") ){
+                    threaddata.put("status" + data[2], data[4]);
+                    threaddata.put("processtime", data[0] + " " + data[1]);
 
+                // continue
+                }else if ( data[4].equals("end") ){
+                    threaddata.put("endcode" + data[2], data[5]);
+                    threaddata.put("endtime", data[0] + " " + data[1]);
+                    // Del after add to file
+                    threaddata.remove(data[2]);
+                    }
+                    // System.out.println(cleandata);
+                    System.out.println(threaddata);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
